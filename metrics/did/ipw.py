390d2d37236738a92084d89b692015e2f4197278
Moritz Helm <moritz.helm@7-tv.de>
2021-11-30 14:09:56 +0100

    Refactor IPW influence function.
    Refactor influence function.
    
    Refactor ipw influence function.

--- metrics/did/ipw.py
+++ metrics/did/ipw.py
@@ -6,12 +6,58 @@ from functools import cached_property
 # Third party
 import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
 
 # First party
 from metrics.logistic_regression import LogisticRegression
 
 
 class IpwDid:
+    def _influence_function_treatment_component(self, data: pd.DataFrame) -> NDArray:
+        return (
+            data["treatment_status"]
+            * (
+                data["outcome"]
+                - (data["treatment_status"] * data["outcome"]).mean()
+                / data["treatment_status"].mean()
+            )
+        ).values.reshape(-1, 1) / data["treatment_status"].mean()
+
+    def _influence_function_control_component_1(self, data: pd.DataFrame, odds: NDArray) -> NDArray:
+        control_group = (1 - data["treatment_status"].values.reshape(-1, 1)) * odds
+        return control_group * (
+            data["outcome"].values.reshape(-1, 1)
+            - (
+                (data["outcome"].values.reshape(-1, 1) * control_group).mean()
+                / control_group.mean()
+            )
+        )
+
+    def _influence_function_control_component_2(self, data: pd.DataFrame, odds: NDArray) -> NDArray:
+        alr = self.selection_model.score @ self.selection_model.vce
+        control_weights = (1 - data["treatment_status"].values.reshape(-1, 1)) * odds
+        control_component = (
+            data["outcome"].values.reshape(-1, 1) * control_weights
+        ).mean() / control_weights.mean()
+        m2 = (
+            control_weights
+            * (data["outcome"].values.reshape(-1, 1) - control_component)
+            * self.selection_model.get_design_matrix(data)
+        )
+        return (alr @ m2.mean(axis=0)).reshape(-1, 1)
+
+    def _influence_function_control_component(self, data: pd.DataFrame, odds: NDArray) -> NDArray:
+        control_weights = (1 - data["treatment_status"].values.reshape(-1, 1)) * odds
+        return (
+            self._influence_function_control_component_1(data, odds)
+            + self._influence_function_control_component_2(data, odds)
+        ) / control_weights.mean()
+
+    def _compute_influence_function(self, data, odds) -> NDArray:
+        return self._influence_function_treatment_component(
+            data
+        ) - self._influence_function_control_component(data, odds)
+
     def __init__(self, formula: str, data: pd.DataFrame):
         # Remark: give in a dataframe with two time periods
         max_time_period = data["time_period"].max()  # noqa
@@ -28,48 +74,16 @@ class IpwDid:
             outcome=lambda df: df["outcome"].values
             - data.loc[data["time_period"] == min_time_period, "outcome"].values
         )
-        self.n_obs = data.shape[0]
-        data_control = data.query("treatment_status==0")
-        data_treated = data.query("treatment_status==1")
         self.n_units = data_selection_model.shape[0]
 
-        self._att = self._compute_att(data_treated, data_control)
+        self._att = self._compute_att(data)
 
-        # Variance
-        alr_ps = self.selection_model.score @ self.selection_model.vce
-        outcome_treated = data_treated["outcome"].values.reshape(-1, 1)
-        outcome_control = data_control["outcome"].values.reshape(-1, 1)
+        odds = self.selection_model.predict_odds(data)
+        self._influence_function = self._compute_influence_function(data, odds)
 
-        influence_function_treat_other = (outcome_treated - outcome_treated.mean()) / data[
-            "treatment_status"
-        ].mean()
-        n_control = data_control.shape[0]
-        odds = self.selection_model.predict_odds(data_control)
-
-        self._influence_function = np.r_[
-            influence_function_treat_other, np.zeros((n_control, 1))
-        ] - (
-            (
-                np.r_[
-                    np.zeros((data_treated.shape[0], 1)),
-                    odds * outcome_control - odds * (odds * outcome_control).mean() / odds.mean(),
-                ]
-                + (
-                    alr_ps
-                    @ np.r_[
-                        np.zeros((data_treated.shape[0], 2)),
-                        (
-                            (outcome_control - ((odds * outcome_control).mean() / odds.mean()))
-                            * odds
-                            * self.selection_model.get_design_matrix(data_control)
-                        ),
-                    ].mean(axis=0)
-                ).reshape(-1, 1)
-            )
-            / np.r_[np.zeros((data_treated.shape[0], 1)), odds].mean()
-        )
-
-    def _compute_att(self, data_treated: pd.DataFrame, data_control: pd.DataFrame) -> float:
+    def _compute_att(self, data: pd.DataFrame) -> float:
+        data_control = data.query("treatment_status==0")
+        data_treated = data.query("treatment_status==1")
         odds = self.selection_model.predict_odds(data_control)
         return (
             data_treated["outcome"].values.reshape(-1, 1).mean()
