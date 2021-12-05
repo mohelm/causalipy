3a4001be4e5acbd0411d67d8c7d42e5b3a21dd16
Moritz Helm <moritz.helm@7-tv.de>
2021-12-05 15:04:33 +0100

    Bring simulation capabilities back.

--- metrics/did/multi_periods.py
+++ metrics/did/multi_periods.py
@@ -10,7 +10,83 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 # First party
+from metrics.custom_types import NDArrayOfFloats
 from metrics.did.doubly_robust import DoublyRobustDid
+
+
+def simulate_data(
+    n_treated_units: int = 4000,
+    n_untreated_units: int = 4000,
+    n_time_periods: int = 4,
+    seed: int | None = None,
+):
+    """
+    Simulate a dataset to demonstrate multi-period analysis.
+
+
+    """
+    time_periods = np.arange(n_time_periods) + 1
+    n_total_units = n_treated_units + n_untreated_units
+    n_total_obs_treated = n_time_periods * n_treated_units
+    n_total_obs_untreated = n_time_periods * n_untreated_units
+
+    rng = np.random.default_rng(seed)
+
+    def _get_controls(mean: int, group_size: int) -> NDArrayOfFloats:
+        return np.repeat(rng.normal(mean, 1, size=group_size), n_time_periods)
+
+    controls = np.r_[_get_controls(0, n_untreated_units), _get_controls(1, n_treated_units)]
+
+    error = rng.normal(0, 1, (n_total_units) * n_time_periods)
+
+    theta = np.repeat(time_periods, n_total_units)
+
+    group = np.append(
+        np.repeat(
+            rng.choice(time_periods, n_treated_units, True),
+            n_time_periods,
+        ),
+        np.zeros(n_total_obs_untreated),
+    )
+
+    y0 = np.repeat(time_periods, n_total_units) * controls + theta + rng.normal(group, 1) + error
+
+    time_period = np.tile(time_periods, n_total_units)
+    treatment_status = np.where(
+        (np.repeat(np.arange(n_total_units), n_time_periods) < n_treated_units),
+        1,
+        0,
+    )
+    y1 = np.r_[
+        y0[:n_total_obs_treated]
+        + np.where(
+            (time_period[:n_total_obs_treated] >= group[:n_total_obs_treated]),
+            time_period[:n_total_obs_treated] - group[:n_total_obs_treated] + 1,
+            0,
+        )
+        + rng.normal(0, 1, n_total_obs_treated)
+        - error[:n_total_obs_treated],
+        np.empty(n_total_obs_untreated) * np.NaN,
+    ]
+
+    outcome_observed = np.where(treatment_status == 1, y1, y0)
+    unit_id = np.r_[np.repeat(np.arange(n_total_units), n_time_periods)]
+
+    # TODO: what is going on with this data+1
+    return (
+        pd.DataFrame(
+            {
+                "treatment_status": treatment_status,
+                "outcome": outcome_observed,
+                "control": controls,
+                "time_period": time_period,
+                "unit_id": unit_id,
+                "group": group,
+            }
+        )
+        .query("group!=1")
+        .sort_values(["unit_id", "time_period"])
+    )
 
 
 class MultiPeriodDid:
@@ -23,6 +99,7 @@ class MultiPeriodDid:
         def _estimate_model(group: int, time: int) -> DoublyRobustDid:
             early_tp = group - 1 if group <= time else time - 1  # noqa
             current_data = data.query("group in (0,@group)  and time_period in (@early_tp,@time)")
+
             return DoublyRobustDid(formula_or, formula_ipw, current_data)
 
         self._estimates = {(g, t): _estimate_model(g, t) for g, t in combinations}
